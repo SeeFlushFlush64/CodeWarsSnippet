e824c46c915c7c44e5bb9acc6f37ee05c4d1ad92
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-01-05 11:56:47 +0800

    added Cyrillic Letters and C is For CodeWars

---
+++ CisForCodeWars/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace CisForCodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Generate(1));
+            
+        }
+        public static string Generate(int size)
+        {
+            char letterC = 'C';
+            int multiplier = 5;
+            int numOfLines = size * multiplier;
+            // for (int i = 0; i < (int) (numOfLines * 0.2); i++)
+            // {
+            //     Console.WriteLine(new string(letterC, numOfLines));
+            // }
+            // for (int i = (int) (numOfLines * 0.2); i < numOfLines * 0.8; i++)
+            // {
+            //     Console.WriteLine(letterC);
+            // }
+            // for (int i = (int) (numOfLines * 0.8); i < numOfLines; i++)
+            // {
+            //     Console.WriteLine(new string(letterC, numOfLines));
+            // }
+            string cLetter = "";
+            for (int i = 0; i < (int) (numOfLines * 0.2); i++)
+            {
+                cLetter += new string(letterC, numOfLines) + '\n';
+            }
+            for (int i = (int) (numOfLines * 0.2); i < numOfLines * 0.8; i++)
+            {
+                cLetter += letterC;
+                cLetter += '\n';
+            }
+            for (int i = (int) (numOfLines * 0.8); i < numOfLines; i++)
+            {
+                cLetter += new string(letterC, numOfLines);
+            }
+            return cLetter;
+        }
+    }
+}
---
+++ CyrillicLetters/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CyrillicLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char myChar = 'a';
+            int charValue = myChar;
+            Console.WriteLine(charValue.Equals(0x0061));
+        }
+    }
+}
