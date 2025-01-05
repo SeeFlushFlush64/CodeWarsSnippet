a9910bc35ac6fc223867397ff095fd5ee6e3ee2c
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-01-05 18:59:24 +0800

    added FindTheDuplicatedNumberInAUnsortedList

---
+++ FindTheDuplicatedNumberInAConsecutiveUnsortedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+namespace FindTheDuplicatedNumberInAConsecutiveUnsortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(FindDup(new int[] { 1, 2, 3, 3, 4, 5 }));
+        }
+        public static int FindDup(int[] arr)
+        { 
+            return arr.GroupBy(num => num).Where(g => g.Count() > 1).Select(num => num.Key).Single();
+        }
+    }
+}
